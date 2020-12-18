50faa87c32e804a64c621a333ec00c37a909edc4
Aleksandr Cuchriajev <76211477+acuchriajev@users.noreply.github.com>
2020-12-19 02:10:26 +1300

    Add files via upload
    Simple counter program

---
+++ WindowsFormsAppSimpleCounter/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppSimpleCounter
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            lblNumber.Text = counter.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (lblNumber.Text.Equals("0")){
+                counter = 0;
+            }
+            else
+            {
+                counter--;
+                lblNumber.Text = counter.ToString();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            lblNumber.Text = "0";
+            lblNumber.Text = counter.ToString();
+        }
+    }
+}
