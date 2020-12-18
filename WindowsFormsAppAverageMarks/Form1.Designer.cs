18917f5ce78355949f790edfeb11a3f998439138
Aleksandr Cuchriajev <76211477+acuchriajev@users.noreply.github.com>
2020-12-19 02:13:43 +1300

    Add files via upload
    Average marks program

---
+++ WindowsFormsAppAverageMarks/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsAppAverageMarks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtStart
+            // 
+            this.BtStart.Location = new System.Drawing.Point(98, 98);
+            this.BtStart.Name = "BtStart";
+            this.BtStart.Size = new System.Drawing.Size(75, 23);
+            this.BtStart.TabIndex = 0;
+            this.BtStart.Text = "Start";
+            this.BtStart.UseVisualStyleBackColor = true;
+            this.BtStart.Click += new System.EventHandler(this.BtStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 193);
+            this.Controls.Add(this.BtStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtStart;
+    }
+}
+
---
+++ WindowsFormsAppAverageMarks/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppAverageMarks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int mark1;
+        int mark2;
+        int mark3;
+        double result;
+
+        private void BtStart_Click(object sender, EventArgs e)
+        {
+            mark1 = int.Parse(Interaction.InputBox("Type the mark1", "Mark1", "", 200));
+            mark2 = int.Parse(Interaction.InputBox("Type the mark2", "Mark2", "", 200));
+            mark3 = int.Parse(Interaction.InputBox("Type the mark3", "Mark3", "", 200));
+            result = (double) (mark1 + mark2 + mark3) / 3;
+            MessageBox.Show("The result is "+ result.ToString());
+        }
+    }
+}
